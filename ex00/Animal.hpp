59e33c4bc5ff01c1e6a9c643058f6fa57f660447
ethanlxz <ethanlxz@gmail.com>
2023-11-27 19:59:37 +0800

    2nd git

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -6,6 +6,13 @@ class Animal
 		std::string type;
 	public:
 		Animal();
+		Animal(const Animal &oldAnimal);
+		Animal	&operator=(const Animal &oldAnimal);
+		virtual	~Animal(void);
+
+		virtual void	makeSound(void);
+		std::string		getType(void);
+
 
 
 };
